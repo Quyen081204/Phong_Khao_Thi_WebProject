3571e315aa3e8c5744cb583a3fd97f9b0feedfc3
trankiet13 <trankiet132004@gmail.com>
2023-08-21 15:35:47 +0700

    Hoan chinh trang chu -1

---
+++ basic.js
@@ -0,0 +1,112 @@
+// GET CURRENT DATE
+var currentDate = new Date();
+
+var day = currentDate.getDay();
+var date = currentDate.getDate();
+var month = currentDate.getMonth();
+var year = currentDate.getFullYear();
+
+// CHANGE DOM
+var currentDay = "";
+var currentMonth = "";
+var updateDate = "";
+var fulldate = "";
+
+var dateBox = document.querySelectorAll(".date-box");
+
+switch (day) {
+    case 0:
+        currentDay = "Sunday" 
+    break;
+    case 1:
+        currentDay = "Monday"  
+    break;
+    case 2:
+        currentDay = "Tuesday"  
+    break;
+    case 3:
+        currentDay = "Wednesday"  
+    break;
+    case 4:
+        currentDay = "Thursday"  
+    break;
+    case 5:
+        currentDay = "Friday"  
+    break;
+    case 6:
+        currentDay = "Saturday"  
+    break;
+} 
+
+
+switch (month) {
+    case 0:
+        currentMonth = "Jan";    
+     break;
+    case 1:
+        currentMonth = "Feb";
+    break;
+    case 2:
+        currentMonth = "Mar";
+    break;
+    case 3:
+        currentMonth = "Apr";
+    break;
+    case 4:
+        currentMonth = "May";
+    break;
+    case 5:
+        currentMonth = "Jun";
+    break;
+    case 6:
+        currentMonth = "Jul";
+    break;
+    case 7:
+        currentMonth = "Aug";
+    break;
+    case 8:
+        currentMonth = "Sep";
+    break;
+    case 9:
+        currentMonth = "Oct";
+    break;
+    case 10:
+        currentMonth = "Nov";
+    break;
+    case 11: 
+        currentMonth = "Dec";
+    break;
+}
+
+// UPDATE
+
+fulldate = currentDay + ", " + currentMonth + " " + date + "th" + ", " + year;
+
+for (var i = 0;i < dateBox.length;i++) {
+    dateBox[i].innerText = fulldate;
+}
+
+// BOX DANG NHAP
+
+const appearance = document.querySelector('.js-appearance')
+const boxxx = document.querySelector('.js-box')
+const boxContainer = document.querySelector('.js-box-container')
+const titles = document.querySelectorAll('.js-title')
+function AppearanceSearchBox () {
+    boxxx.classList.add('open')
+}
+function CloseSearchBox()
+{
+    boxxx.classList.remove('open')
+}
+function Stop (event)
+{
+    event.stopPropagation()
+}
+appearance.addEventListener('click', AppearanceSearchBox)
+boxxx.addEventListener('click', CloseSearchBox)
+boxContainer.addEventListener('click', Stop)
+for ( const title of titles )
+{
+    title.addEventListener('click', AppearanceSearchBox)
+}
